df5104b715a85fb476d85e81e18b79c0f3f10357
Nikita <nonamekuden@gmail.com>
2021-05-28 18:22:27 +0300

    Start of prject. Created apiService and api config

---
+++ src/js/config/apiConfig.js
@@ -0,0 +1,12 @@
+
+
+const config = {
+    url: 'https://aviasales-api.herokuapp.com'
+}
+
+
+
+
+
+//експортуємо об'єкт конфіга
+export default config;
---
+++ src/js/services/apiService.js
@@ -0,0 +1,39 @@
+import config from '../config/apiConfig'
+import axios from 'axios' // плагин полегшує реалізацію запитів на сервер
+
+//класс з набором методыв для взаємодії с сервером
+
+// Ендпоінти сервера
+// /countries -- массив країн
+// /cities -- массив міст
+// /prices/cheap -- массив с доступними авіарейсами
+class Api{
+    constructor(config){
+        this.url = config.url;
+    }
+    async countries() {
+        // ця асинхронна функція виконує запит на сервер для отримання міст
+        try {
+            const response = await axios.get(`${this.url}/countries`);
+            return response.data;
+        } catch(err){
+            console.log(err);
+            return Promise.reject(err);
+        }
+    }
+    async cities(){
+         // ця асинхронна функція виконує запит на сервер для отримання країн
+         try {
+            const response = await axios.get(`${this.url}/cities`);
+            return response.data;
+        } catch(err){
+            console.log(err);
+            return Promise.reject(err);
+        }
+    }
+    prices(params){}
+}
+
+const api = new Api(config);
+
+export default api
